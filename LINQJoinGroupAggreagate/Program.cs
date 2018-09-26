a972240ec9ee1114a570699212b7828480922863
Fredrik Haglund <mejl@fredrikhaglund.se>
2018-09-26 12:02:13 +0200

    Read data file and create objects

--- LINQJoinGroupAggreagate/Program.cs
+++ LINQJoinGroupAggreagate/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,65 @@ namespace LINQJoinGroupAggreagate
     {
         static void Main(string[] args)
         {
+            //TextDemo();
+
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Fel. Använd programmet genom att skriva LINQJoinGroupAggreagate.exe <filnamn>");
+                return;
+            }
+
+            var filnamn = args[0];
+
+            var bank = ReadBankData(filnamn);
+
+            var countOfCustomers = bank.Customers.Where(c => c.Country == "Sweden").Count();
+
+        }
+
+        private static Bank ReadBankData(string path)
+        {
+            var bank = new Bank();
+
+            using (var reader = new StreamReader(path))
+            {
+                int countOfCustomers = int.Parse(reader.ReadLine());
+
+                for (int i = 0; i < countOfCustomers; i++)
+                {
+                    string line = reader.ReadLine();
+                    string[] columns = line.Split(';');
+
+                    var customer = new Customer
+                    {
+                        ID = int.Parse(columns[0]),
+                        Name = columns[2],
+                        Country = columns[7]
+                    };
+
+                    bank.Customers.Add(customer);
+                }
+            }
+
+
+            // Vi använder using istället för att garanterat stänga filen.
+            // reader.Close(); // Stänger filen - lämnar tillbaka resursen till operativsystemet.
+
+
+            return bank;
+        }
+
+        private static void TextDemo()
+        {
+            string text = "Hej jag heter Fredrik";
+
+            string baklänges = new string(text.Reverse().ToArray());
+
+            var rövare = String.Join("", text.Select(
+                    bokstav => "aoueiäåöy ".Contains(bokstav) ? bokstav.ToString() : bokstav + "o" + bokstav
+                ));
+
+            Console.WriteLine(rövare);
         }
     }
 }
